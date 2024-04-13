da4496c38387180a440eac8043889df14a2a9bca
Justin Laws <jayapimtesting@outlook.com>
2024-04-13 11:24:49 -0400

    aks-demo
    aks-demo

---
+++ server.js
@@ -0,0 +1,10 @@
+  // server.js
+  'use strict';
+  var express = require('express');
+  var app = express();
+  app.get('/', function (req, res) {
+    res.send('ACR Test for Cloudnoobs.com');
+  });
+  app.listen(3000, function () {
+    console.log('Example app listening on port 3000!');
+  });
