405354a14242825d0fd4eac227f6a05c1d0f18d6
Justin Laws <86179207+sampleworthy@users.noreply.github.com>
2024-04-21 20:20:50 -0400

    Update server.js

--- server.js
+++ server.js
@@ -3,7 +3,7 @@
   var express = require('express');
   var app = express();
   app.get('/', function (req, res) {
-    res.send('AKS Test for sas');
+    res.send('AKS Test for demo');
   });
   app.listen(3000, function () {
     console.log('Example app listening on port 3000!');
